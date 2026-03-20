c5c7af84b04ab23be8b077fd338708f38cefa748
Krunalraut27 <137792702+Krunalraut27@users.noreply.github.com>
2026-03-20 22:29:51 +0530

    Sorted code and resolved complications

---
+++ src/main/java/com/student/management/Repository/CourseRepository.java
@@ -0,0 +1,11 @@
+package com.student.management.Repository;
+
+
+import com.student.management.Entity.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CourseRepository extends JpaRepository<Course,Long> {
+    List<Course> findByDeletedFalse();
+}
