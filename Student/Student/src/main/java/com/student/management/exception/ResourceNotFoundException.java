ef66e415fcfd11cedf280f2cb688c03641cab8e9
Krunalraut27 <137792702+Krunalraut27@users.noreply.github.com>
2026-03-20 22:07:34 +0530

    Delete Student/Student directory

--- Student/Student/src/main/java/com/student/management/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.student.management.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message){
-        super(message);
-    }
-}
