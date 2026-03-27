14ba752962a0554a34a563e3deac992b914c5a99
Pratiksha Krishna Gavhane <165024468+Gavhane-22@users.noreply.github.com>
2026-03-27 14:00:46 +0530

    Add files via upload

---
+++ src/main/java/com/student/management/Service/CourseService.java
@@ -0,0 +1,19 @@
+package com.student.management.service;
+
+import com.student.management.entity.Course;
+
+import java.util.List;
+
+
+public interface CourseService  {
+
+
+    Course createCourse(Course course);
+
+    List<Course> getAllCourses();
+
+    Course getCourseById(Long id);
+
+    Course updateCourse(Long id, Course course);
+
+    void softDeleteCourse(Long id);}
